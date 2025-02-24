c155260f070f6286d9e144f66a6c79b0bf337efe
EL BOUZIYANI Anas <aaelbouziyani@gmail.com>
2025-02-24 10:50:28 +0000

    first commit

---
+++ src/main/java/ma/ensias/miniprojet/HelloBean.java
@@ -0,0 +1,18 @@
+package ma.ensias.miniprojet;
+
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Named;
+
+@Named
+@RequestScoped
+public class HelloBean {
+    private String message = "Hello, World!";
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
